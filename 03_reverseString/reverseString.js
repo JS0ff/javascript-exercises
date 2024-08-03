4e871b0bd1c6345d4741a6eb46d63406b0b43139
Asan <onlylearningjs@gmail.com>
2024-08-02 22:03:01 -0700

    Complete first test, add index for console

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  numOfChar = word.length;
+  let sum = "";
+  for (let i = numOfChar - 1; i >= 0; i--) {
+    sum += word[i];
+  }
+  return sum;
 };
-
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
