76594fd9a299b11c76b503414563c4593a3675b2
Asan <onlylearningjs@gmail.com>
2024-08-03 20:39:43 -0700

    Remove one value from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+"use strict";
+const removeFromArray = function (array, number) {
+  let lenArray = array.length;
+  for (let i = 0; i < lenArray; i++) {
+    if (number === array[i]) {
+      console.log(`Found!! It is ${array[i]}`);
+      array.splice(i, 1);
+      return array;
+    }
+  }
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
