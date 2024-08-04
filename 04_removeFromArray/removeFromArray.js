ceb56bd8c6c925484b297c7014d13528eb30db55
Asan <onlylearningjs@gmail.com>
2024-08-03 20:53:20 -0700

    Remove two numbers from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,18 @@
 "use strict";
-const removeFromArray = function (array, number) {
+const removeFromArray = function (array, number, secondNumber) {
   let lenArray = array.length;
+
   for (let i = 0; i < lenArray; i++) {
+    if (secondNumber === array[i]) {
+      array.splice(i, 1);
+    }
     if (number === array[i]) {
       console.log(`Found!! It is ${array[i]}`);
       array.splice(i, 1);
-      return array;
     }
   }
+  return array;
 };
-console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
