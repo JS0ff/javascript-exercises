bff4e02c49caa5ef998d92dc4072f8644484be77
Asan <onlylearningjs@gmail.com>
2024-08-08 08:01:14 -0700

    Complete 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,34 @@
 "use strict";
-const removeFromArray = function (array, first, second) {
+const removeFromArray = function (array, ...numbers) {
+  // const newArray = [];
+  // array.forEach((item) => {
+  //   if (!numbers.includes(item)) {  <----- More easier
+  //     newArray.push(item);
+  //   }
+  // });
+  // return newArray;
+
   let lenArray = array.length;
 
+  numbers = numbers.sort((a, b) => a - b); // Sorts values inside numbers from low to higher values
+  console.log(numbers);
+
   for (let i = 0; i < lenArray; i++) {
-    if (second === array[i]) {
-      console.log(`Found it! ${array[i]}`);
-      array.splice(i, 1);
-      console.log(array);
-      // Checks if the next value in array has the same value
-      if (second === array[i]) {
-        array.splice(i, 1);
+    for (let j = 0; j <= array[i]; j++) {
+      if (array[0] === numbers[0]) {
+        array.splice(0, 1);
       }
-    }
-    // Copy of the above condition but for the first value in funciton
-    if (first === array[i]) {
-      console.log("Found it!!!", `${array[i]}`);
-      array.splice(i, 1);
-      if (first === array[i]) {
+      if (array[i] === numbers[j]) {
         array.splice(i, 1);
+        if (array[i] === numbers[j]) {
+          array.splice(i, 1);
+        }
       }
     }
   }
   return array;
 };
-console.log(removeFromArray([1, 2, 2, 3], 2));
+
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
 // Do not edit below this line
 module.exports = removeFromArray;
