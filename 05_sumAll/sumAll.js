40578ce51fc32db241b31832f04447e93c023622
Asan <onlylearningjs@gmail.com>
2024-08-08 08:44:13 -0700

    Create an value exceptions and restrictions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,29 @@
 const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    start % 1 !== 0 ||
+    end % 1 !== 0 ||
+    typeof start !== "number" ||
+    typeof end !== "number"
+  ) {
+    return "ERROR";
+  }
+
+  if (start > end) {
+    biggerNum = start;
+    lowerNum = end;
+  } else if (end > start) {
+    biggerNum = end;
+    lowerNum = start;
+  }
   let sum = 0;
-  for (let i = start; i <= end; i++) {
+  for (let i = lowerNum; i <= biggerNum; i++) {
     sum += i;
   }
   return sum;
 };
-console.log(sumAll(1, 4));
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
