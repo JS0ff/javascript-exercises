f3239b9111277fea345d4f7d4c731da107896561
Asan <onlylearningjs@gmail.com>
2024-08-08 18:56:58 -0700

    Create convert to Celsius and add formula

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  let tempInC = ((tempInF - 32) * 5) / 9;
+  return Number(tempInC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  let tempInF = (tempInC * 5) / 9 + 32;
+  return tempInF;
 };
 
+console.log(typeof convertToCelsius(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
