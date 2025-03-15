f734c1c6721af656d7b5b7b7d9cbd77d2e0d24e9
Asan <onlylearningjs@gmail.com>
2025-03-15 07:42:46 +0600

    Create reverse loop to sum up characters in the word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let finalWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    finalWord += word[i];
+  }
+  return finalWord;
 };
 
 // Do not edit below this line
