2dad5b219a24377b56aba9951481f306a8e55d1a
Asan <onlylearningjs@gmail.com>
2025-03-17 18:43:44 +0600

    Use the formula to get celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
-};
-
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  let celsius = ((temp - 32) * 5) / 9;
+  if (!Number.isInteger(celsius)) {
+    celsius = Number(celsius.toFixed(1));
+  }
+  return celsius;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
