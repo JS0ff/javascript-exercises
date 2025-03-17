3a28660e2d34cce670bc100f78ad8b00282d9ca4
Asan <onlylearningjs@gmail.com>
2025-03-17 18:03:42 +0600

    Find century with leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // Finds out century leap year
+  if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+    // Finds out century but not leap year
+  } else if (year % 100 === 0 && year % 400 != 0) {
+    return false;
+    // Finds out any calendar leap year
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
