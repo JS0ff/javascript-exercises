21334b44d542e627ec8ff62830df6bda7fab26fb
Asan <onlylearningjs@gmail.com>
2025-03-17 12:17:21 +0600

    Create loop to iterate through values to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...arrayVar) {
+  //Loop to iterate through user array
+  outer: for (let i = 0; i < array.length; i++) {
+    //Loop to iterate through array with values to remove
+    for (let j = 0; j < arrayVar.length; j++) {
+      if (array[i] === arrayVar[j]) {
+        console.log("Before Splice: " + array);
+        array.splice(i, 1);
+        i = i - 1;
+        console.log("After Splice: " + array);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
