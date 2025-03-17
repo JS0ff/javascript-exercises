cade71b659508e104e4f5a359954e0ec3b589a23
Asan <onlylearningjs@gmail.com>
2025-03-17 17:53:39 +0600

    Sum all numbers in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sum = 0;
+  if (!(start % 1 === 0) || !(end % 1 === 0)) {
+    return "ERROR";
+  } else if (typeof start != "number" || typeof end != "number") {
+    return "ERROR";
+  } else if (start < 0 || end < 0) {
+    return "ERROR";
+  } else if (start > end) {
+    let biggerValue = start;
+    start = end;
+    end = biggerValue;
+  }
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
