372881038d6d756cc7a34f4b3ba402c773d6fa2a
Asan <onlylearningjs@gmail.com>
2025-04-08 18:35:52 +0600

    Use the reduce method to loop through an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,17 @@
-const add = function() {
-	
+const add = function (arr) {
+  sum = arr.reduce((sum, item) => (sum += item));
+  return sum;
 };
 
-const subtract = function() {
-	
-};
+const subtract = function () {};
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +20,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
