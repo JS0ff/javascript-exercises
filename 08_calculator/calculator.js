983696cd879388fe0dac0e42c9efade0f8ed460e
Asan <onlylearningjs@gmail.com>
2025-04-08 19:12:35 +0600

    Create power function that powers two numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,7 +19,9 @@ const multiply = function (arr) {
   return (result = arr.reduce((product, item) => (product *= item)));
 };
 
-const power = function (a, b) {};
+const power = function (a, b) {
+  return a ** b;
+};
 
 const factorial = function () {};
 
