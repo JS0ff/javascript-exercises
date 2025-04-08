ad173e3ef5dd6813f30485c0fc3df827e2f50ab5
Asan <onlylearningjs@gmail.com>
2025-04-08 23:04:20 +0600

    Make code shorter

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,18 +1,15 @@
 const add = function (a, b) {
   arr = [a, b];
-  let sum = arr.reduce((sum, item) => (sum += item));
-  return sum;
+  return arr.reduce((sum, item) => (sum += item));
 };
 
 const subtract = function (a, b) {
   arr = [a, b];
-  let result = arr.reduce((sum, item) => (sum -= item));
-  return result;
+  return arr.reduce((sum, item) => (sum -= item));
 };
 
 const sum = function (arr) {
-  let sum = arr.reduce((sum, item) => (sum += item), 0);
-  return sum;
+  return arr.reduce((sum, item) => (sum += item), 0);
 };
 
 const multiply = function (arr) {
