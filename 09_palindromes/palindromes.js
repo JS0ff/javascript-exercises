44a359952f6f3b34557fb895257a4748d8f3df08
Asan <onlylearningjs@gmail.com>
2025-04-09 09:58:10 +0600

    Check every element in arr that have ! sign

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Create an array of string elements
+  // all items are lowercase
+  let arr = string.toLowerCase().split("");
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "!") {
+      arr.splice(i, 1);
+    }
+  }
+  console.log(arr);
+
+  let reverseArr = arr.map((item) => item).reverse();
+
+  // Check if words are similar;
+  if (arr.join("") === reverseArr.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
