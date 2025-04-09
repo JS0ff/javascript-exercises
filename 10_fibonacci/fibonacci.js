e2b79d9e22e086cf27faf93e82f4bac320a32b6a
Asan <onlylearningjs@gmail.com>
2025-04-09 20:39:22 +0600

    Create comments to conditionals

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -6,19 +6,15 @@ const fibonacci = function (number) {
   number = Number(number);
 
   // 0's element of an fibonacci sequence is 0;
-  if (number === 0) {
-    return 0;
-  } else if (number < 0) {
-    // Return string if negative user number
-    return "OOPS";
-  }
+  if (number === 0) return 0;
+  // Return string if negative user number
+  else if (number < 0) return "OOPS";
+
   //Full array elements with fibonacci sequence
-  for (let i = 0; i < number; i++) {
-    if (i === 0 || i === 1) {
-      arr.push(1);
-    } else if (i > 1) {
-      arr.push(arr[i - 1] + arr[i - 1 - 1]);
-    }
+  arr.push(1, 1); //First two elements are 1,1
+
+  for (let i = 2; i < number; i++) {
+    arr.push(arr[i - 1] + arr[i - 1 - 1]);
   }
   // Return user value of fibonacci
   return arr[arr.length - 1]; // return last item
