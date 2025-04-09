43a98fce35eb1169cc6032f2d50097eb5a23d86a
Asan <onlylearningjs@gmail.com>
2025-04-09 18:38:17 +0600

    Create fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  const arr = [];
+  for (let i = 0; i < number; i++) {
+    if (i === 0) {
+      arr.push(1);
+    } else if (i === 1) {
+      arr.push(1);
+    } else if (i > 1) {
+      arr.push(arr[i - 1] + arr[i - 1 - 1]);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
