f53f5076ea840a62d99f2ac367398d52cc49bd97
Asan <onlylearningjs@gmail.com>
2025-04-10 15:11:48 +0600

    Create yearOfDeath equal to present year if no yearOfDeath property

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,13 @@
 const findTheOldest = function (obj) {
-  oldest = obj.reduce(
-    (oldest, item) =>
-      oldest < item.yearOfDeath - item.yearOfBirth
-        ? (oldest = item.yearOfDeath - item.yearOfBirth)
-        : oldest,
-    0
+  //Present year
+  let presentYear = new Date().getFullYear();
+  // If there is no yearOfDeath the person yearOfDeath is equal to presentYear
+  obj.map((item) =>
+    item.yearOfDeath === undefined
+      ? (item.yearOfDeath = presentYear)
+      : item.yearOfDeath
   );
-  return obj.filter(
-    (item) => item.yearOfDeath - item.yearOfBirth === oldest
-  )[0];
+  return obj;
 };
 
 // Do not edit below this line
